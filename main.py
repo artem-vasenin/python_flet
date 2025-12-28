5a699c5cfbb11d5531b807d3a0200ca7cd2f05f6
Rusich <vasenin@verqor.com>
2025-12-28 23:16:21 +0300

    added weather app

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 import flet as ft
+import requests
+
 
 def main(page: ft.Page):
+    API = 'faffcba6653d0e42a641ceeff885620f'
     page.theme_mode = 'dark'
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.title = 'Rusich App'
+    page.title = 'Погодная приложуха'
 
-    field_label = ft.Text('SuperMega Text', color='#ff7700', size=22)
-    form_field = ft.TextField('0', width=150, text_align=ft.TextAlign.CENTER, border_color='#ff7700')
+    user_data = ft.TextField('', width=400, label='Введите город', border_color='#bbbbbb')
+    weather_data = ft.Text('')
 
-    def get_info(e):
-        field_label.value = f'Total: {form_field.value}'
+    def get_user_data(e):
+        if len(user_data.value) < 2 or len(user_data.value) > 20:
+            return
+
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={user_data.value}&appid={API}&units=metric'
+        res = requests.get(url).json()
+
+        if not res or res['cod'] == '404':
+            weather_data.value = 'Данные по этому городу не найдены...'
+            page.update()
+            return
+
+        temp = res['main']['temp']
+        wind = res['wind']['speed']
+        weather_data.value = f'Температура: {temp}°C, Скорость ветра: {wind}км/ч'
         page.update()
 
-    def clear(e):
-        field_label.value = 'SuperMega Text'
-        form_field.value = '0'
+    def on_theme_change():
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
         page.update()
 
     page.add(
-        ft.Row(
-            controls=[
-                ft.IconButton(ft.icons.Icons.HOME, on_click=get_info),
-                ft.Icon(ft.icons.Icons.ARROW_BACK),
-                ft.ElevatedButton('Clear', on_click=clear),
-                ft.Checkbox(label='Check it', value=False)
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        ft.Row(
-            controls=[
-                field_label,
-                form_field,
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
+        ft.Row([
+            ft.IconButton(ft.icons.Icons.SUNNY, on_click=on_theme_change),
+            ft.Text(page.title, color='#ff7700')
+        ], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([user_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([weather_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Divider(height=10, color="transparent"),
+        ft.Row([
+            ft.Button('Показать', on_click=get_user_data)
+        ], alignment=ft.MainAxisAlignment.CENTER),
     )
 
 ft.run(main=main)
