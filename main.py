3922995cc75af9ab68b81104574bdaa18d305aef
Rusich <vasenin@verqor.com>
2025-12-28 20:10:22 +0300

    added base app

--- main.py
+++ main.py
@@ -5,13 +5,35 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.title = 'Rusich App'
 
+    field_label = ft.Text('SuperMega Text', color='#ff7700', size=22)
+    form_field = ft.TextField('0', width=150, text_align=ft.TextAlign.CENTER, border_color='#ff7700')
+
+    def get_info(e):
+        field_label.value = f'Total: {form_field.value}'
+        page.update()
+
+    def clear(e):
+        field_label.value = 'SuperMega Text'
+        form_field.value = '0'
+        page.update()
+
     page.add(
         ft.Row(
             controls=[
-                ft.IconButton(ft.icons.Icons.HOME),
+                ft.IconButton(ft.icons.Icons.HOME, on_click=get_info),
                 ft.Icon(ft.icons.Icons.ARROW_BACK),
+                ft.ElevatedButton('Clear', on_click=clear),
+                ft.Checkbox(label='Check it', value=False)
             ],
-            alignment=ft.MainAxisAlignment.CENTER)
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row(
+            controls=[
+                field_label,
+                form_field,
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
     )
 
-ft.app(target=main)
+ft.run(main=main)
